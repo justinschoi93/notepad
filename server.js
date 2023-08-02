130c409a786d3ba4b83c9503dd1351c245636441
justinschoi93 <justinschoi93@gmail.com>
2023-08-01 23:03:38 -0700

    port number changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const api = require('./routes/index.js');
 const path = require('path');
 // const { clog } = require('./middleware/clog');
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.port || 5001;
 
 const app = express();
 
